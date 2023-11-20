7288e5b3459078e58d85680c910b6d7c39c85a44
Zuong <quocvuongle.ct@gmail.com>
2023-11-20 23:01:45 +0700

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { Button } from "@chakra-ui/button";
+import { Container } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+import Header from "./components/Header";
 function App() {
     return (
-        <div>
-            <Button>hello</Button>
-        </div>
+        <Container maxW="620px">
+            <Header />
+            <Routes>
+                <Route path="/:username" element={<UserPage />}></Route>
+                <Route path="/:username/post/:pid" element={<PostPage />}></Route>
+            </Routes>
+        </Container>
     );
 }
 

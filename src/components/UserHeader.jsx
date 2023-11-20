26a3c103553108f6c621f513b5c92638d8c3dd3f
Zuong <quocvuongle.ct@gmail.com>
2023-11-20 23:02:52 +0700

    add user header component

---
+++ src/components/UserHeader.jsx
@@ -0,0 +1,99 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Box, VStack, Flex, Text, Link } from "@chakra-ui/layout";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import { useToast } from "@chakra-ui/react";
+import { Portal } from "@chakra-ui/portal";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+const UserHeader = () => {
+    const toast = useToast();
+
+    /* copy function when click on button */
+    const copyURL = () => {
+        const currentURL = window.location.href;
+        navigator.clipboard.writeText(currentURL).then(() => {
+            toast({
+                title: "Profile copied.",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+            });
+        });
+    };
+
+    return (
+        <VStack gap={4} alignItems={"start"}>
+            <Flex justifyContent={"space-between"} w={"full"}>
+                <Box>
+                    <Text fontSize={"2xl"} fontWeight={"bold"}>
+                        Quoc Zuong
+                    </Text>
+                    <Flex gap={2} alignItems={"center"}>
+                        <Text fontSize={"sm"}>zuong</Text>
+                        <Text fontSize={"xs"} bg={"gray.dark"} color={"gray.light"} p={1} borderRadius={"full"}>
+                            threads.net
+                        </Text>
+                    </Flex>
+                </Box>
+                <Box>
+                    <Avatar
+                        size={{
+                            base: "md",
+                            md: "xl",
+                        }}
+                        name={"Zuong"}
+                        src="/zuck-avatar.png"
+                    />
+                </Box>
+            </Flex>
+
+            <Text>adlkfjasldfkj asdlkfj aslkdjf laskdjf</Text>
+            <Flex w={"full"} justifyContent={"space-between"}>
+                <Flex gap={2} alignItems={"center"}>
+                    <Text color={"gray.light"}>3.2k followers</Text>
+                    <Box w={1} h={1} bg={"gray.light"} borderRadius={"full"}></Box>
+                    <Link color={"gray.light"} cursor={"pointer"}>
+                        instagram.com
+                    </Link>
+                </Flex>
+                <Flex gap={2} alignItems={"center"}>
+                    <Box className="icon-container">
+                        <BsInstagram size={24} cursor={"pointer"} />
+                    </Box>
+                    <Box className="icon-container">
+                        <Menu>
+                            <MenuButton>
+                                <CgMoreO size={24} cursor={"pointer"} />
+                            </MenuButton>
+                            <Portal>
+                                <MenuList bg={"gray.dark"}>
+                                    <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                                        Copy link
+                                    </MenuItem>
+                                </MenuList>
+                            </Portal>
+                        </Menu>
+                    </Box>
+                </Flex>
+            </Flex>
+
+            <Flex w={"full"}>
+                <Flex flex={1} borderBottom={"1px solid white"} justifyContent={"center"} pb={3} cursor={"pointer"}>
+                    <Text fontWeight={"bold"}>Threads</Text>
+                </Flex>
+                <Flex
+                    flex={1}
+                    borderBottom={"1px solid gray"}
+                    justifyContent={"center"}
+                    color={"gray.light"}
+                    pb={3}
+                    cursor={"pointer"}
+                >
+                    <Text fontWeight={"bold"}>Replies</Text>
+                </Flex>
+            </Flex>
+        </VStack>
+    );
+};
+
+export default UserHeader;

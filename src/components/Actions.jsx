0659ca0523d9abc9531fc20efb97c93a23267ca4
Zuong <quocvuongle.ct@gmail.com>
2023-11-21 11:52:24 +0700

    update size for icons

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -4,16 +4,15 @@ import { Flex } from "@chakra-ui/react";
 const Actions = ({ liked, setLiked }) => {
     return (
         <Flex flexDirection="column">
-            <Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
+            <Flex gap={3} my={1} onClick={(e) => e.preventDefault()} className="">
                 <svg
                     aria-label="Like"
                     color={liked ? "rgb(237, 73, 86)" : ""}
                     fill={liked ? "rgb(237, 73, 86)" : "transparent"}
-                    height="19"
                     role="img"
                     viewBox="0 0 24 22"
-                    width="20"
                     onClick={() => setLiked(!liked)}
+                    className="icon-container"
                 >
                     <path
                         d="M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z"
@@ -22,7 +21,7 @@ const Actions = ({ liked, setLiked }) => {
                     ></path>
                 </svg>
 
-                <svg aria-label="Comment" color="" fill="" height="20" role="img" viewBox="0 0 24 24" width="20">
+                <svg aria-label="Comment" color="" fill="" role="img" viewBox="0 0 24 24" className="icon-container">
                     <title>Comment</title>
                     <path
                         d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
@@ -48,10 +47,9 @@ const RepostSVG = () => {
             aria-label="Repost"
             color="currentColor"
             fill="currentColor"
-            height="20"
             role="img"
             viewBox="0 0 24 24"
-            width="20"
+            className="icon-container"
         >
             <title>Repost</title>
             <path
@@ -68,10 +66,9 @@ const ShareSVG = () => {
             aria-label="Share"
             color=""
             fill="rgb(243, 245, 247)"
-            height="20"
             role="img"
             viewBox="0 0 24 24"
-            width="20"
+            className="icon-container"
         >
             <title>Share</title>
             <line

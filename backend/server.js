cd715db4df189d8f7fe1a866461f09dc9302b365
Zuong <quocvuongle.ct@gmail.com>
2023-11-21 23:13:32 +0700

    update body parser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
 
 connectDB();
 
@@ -7,9 +9,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
-app.get("/", (req, res) => {
-    res.send("h1");
-});
+app.use(express.json()); // to parse json data from req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Hey server started at port ${PORT}!`);

28c0885b49e6b55cd7cfe993b0645f8103d09ac8
Zuong <quocvuongle.ct@gmail.com>
2023-11-21 23:28:34 +0700

    add login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signupUser } from "../controllers/userController.js";
+import { signupUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
 export default router;

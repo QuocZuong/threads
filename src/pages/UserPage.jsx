bc3cea30e32f8da1206dddeb228047a7a1d27564
Zuong <quocvuongle.ct@gmail.com>
2023-11-21 11:52:43 +0700

    update images userpage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,7 +5,7 @@ const UserPage = () => {
     return (
         <>
             <UserHeader />
-            <UserPost likes={1200} replies={320} postImg="/post11.png" postTitle="This is my first post" />
+            <UserPost likes={1200} replies={320} postImg="/post1.png" postTitle="This is my first post" />
             <UserPost likes={1200} replies={320} postImg="/post2.png" postTitle="Hello fiends" />
             <UserPost likes={1100} replies={3320} postImg="/post3.png" postTitle="Zuong day" />
             <UserPost likes={16700} replies={2345} postImg="/post1.png" postTitle="T1 mai dinh" />

c4979465abe5ee37a86c805887664c8da37ae6e0
Zuong <quocvuongle.ct@gmail.com>
2023-11-22 22:54:54 +0700

    create auth page

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,10 @@
+import { useRecoilValue } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
+import Login from "../components/LoginCard";
+import SignupCard from "../components/SignupCard";
+const AuthPage = () => {
+    const authScreenState = useRecoilValue(authScreenAtom);
+    return <>{authScreenState === "login" ? <Login /> : <SignupCard />}</>;
+};
+
+export default AuthPage;

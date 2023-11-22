ab25c3ba7b80cffeb08c275a3513ea6dfbc617b7
Zuong <quocvuongle.ct@gmail.com>
2023-11-22 09:13:36 +0700

    add two new routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { signupUser, loginUser } from "../controllers/userController.js";
+import { signupUser, loginUser, logoutUser, followUnFollowUser } from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout", protectRoute, logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser);
 
 export default router;

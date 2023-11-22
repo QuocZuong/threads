2a09046c48146a7f29b3e6975db6c3eb1ffc4c01
Zuong <quocvuongle.ct@gmail.com>
2023-11-22 20:04:53 +0700

    done reply to post API

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { createPost, getPost, deletePost, likeUnlikePost } from "../controllers/postController.js";
+import {
+    createPost,
+    getPost,
+    deletePost,
+    likeUnlikePost,
+    replyToPost,
+    getFeedPost,
+} from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
+router.get("/feed", protectRoute, getFeedPost);
 router.post("/", protectRoute, createPost);
 router.get("/:id", protectRoute, getPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 
 export default router;

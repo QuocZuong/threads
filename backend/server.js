abd6f509266c09a8a9077bf6f870327f97747241
Zuong <quocvuongle.ct@gmail.com>
2023-11-23 14:59:53 +0700

    set up cloudinary

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 connectDB();
 
@@ -10,8 +11,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
-app.use(express.json()); // to parse json data from req.body
-app.use(express.urlencoded({ extended: true }));
+// Cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Middleware
+app.use(express.json({ limit: "10mb" })); // to parse json data from req.body
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser()); // to parse cookies from req.cookies
 
 // Routes

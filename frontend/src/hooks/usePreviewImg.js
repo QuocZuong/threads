b3f49371b613d6fafbce076db981626c929e88cc
Zuong <quocvuongle.ct@gmail.com>
2023-11-23 15:01:32 +0700

    create custom hook usePreviewImg

---
+++ frontend/src/hooks/usePreviewImg.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import useShowToast from "./useShowToast";
+const usePreviewImg = () => {
+    const [imgUrl, setImgUrl] = useState(null);
+    const showToast = useShowToast();
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file?.type.startsWith("image/")) {
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                setImgUrl(reader.result);
+            };
+
+            // content of the file will be converted to a base64-encoded string representation of the file's data
+            reader.readAsDataURL(file);
+        } else {
+            showToast("File is not an image", "Please select an image file", "error");
+            setImgUrl(null);
+        }
+    };
+
+    return { handleImageChange, imgUrl };
+};
+
+export default usePreviewImg;

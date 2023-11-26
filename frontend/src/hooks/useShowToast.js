a20908cd786a185e9f15a2523dded6558a37a03b
Zuong <quocvuongle.ct@gmail.com>
2023-11-26 10:26:30 +0700

    optimize code

--- frontend/src/hooks/useShowToast.js
+++ frontend/src/hooks/useShowToast.js
@@ -1,17 +1,21 @@
 import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
 
 const useShowToast = () => {
     const toast = useToast();
 
-    const showToast = (title, description, status) => {
-        toast({
-            title,
-            description,
-            status,
-            duration: 3000,
-            isClosable: true,
-        });
-    };
+    const showToast = useCallback(
+        (title, description, status) => {
+            toast({
+                title,
+                description,
+                status,
+                duration: 3000,
+                isClosable: true,
+            });
+        },
+        [toast],
+    );
     return showToast;
 };
 

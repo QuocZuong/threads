fb0347ac416c8fa32b6aefface0a2d12213b128b
Zuong <quocvuongle.ct@gmail.com>
2023-11-26 10:27:21 +0700

    done follow/unfollow

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -1,19 +1,65 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, VStack, Flex, Text, Link } from "@chakra-ui/layout";
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
-import { useColorModeValue } from "@chakra-ui/react";
+import { useColorModeValue, Button } from "@chakra-ui/react";
 import { Portal } from "@chakra-ui/portal";
 import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
 import useShowToast from "../hooks/useShowToast";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
+
 const UserHeader = ({ user }) => {
     const showToast = useShowToast();
+    const currentUser = useRecoilValue(userAtom);
+    const [following, setFollowing] = useState(user?.followers.includes(currentUser._id));
+    const [updating, setUpdating] = useState(false);
+
     /* copy function when click on button */
     const copyURL = () => {
         const currentURL = window.location.href;
         navigator.clipboard.writeText(currentURL).then(() => {
             showToast("Profile copied.", "", "success");
         });
+    };
+
+    const handleFollowUnfollow = async () => {
+        if (!currentUser) {
+            showToast("Error", "Please login to follow", "error");
+            return;
+        }
+        setUpdating(true);
+        try {
+            const res = await fetch(`/api/users/follow/${user._id}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const data = await res.json();
+
+            if (data.error) {
+                showToast("An error occurred.", data.error, "error");
+                return;
+            }
+
+            if (following) {
+                showToast("Success", `Unfollowed ${user.name}`, "success");
+                user.followers.pop();
+            } else {
+                showToast("Success", `Followed ${user.name}`, "success");
+                user.followers.push(currentUser._id);
+            }
+
+            setFollowing((prev) => !prev);
+        } catch (error) {
+            showToast("Error", error, "error");
+        } finally {
+            setUpdating(false);
+        }
     };
 
     return (
@@ -42,13 +88,26 @@ const UserHeader = ({ user }) => {
                             base: "md",
                             md: "xl",
                         }}
-                        name={"Zuong"}
+                        name={user?.name}
                         src={user?.profilePic}
                     />
                 </Box>
             </Flex>
 
             <Text>{user?.bio}</Text>
+
+            {currentUser._id === user._id && (
+                <Link as={RouterLink} to="/update">
+                    <Button>Update profile</Button>
+                </Link>
+            )}
+
+            {currentUser._id !== user._id && (
+                <Button onClick={handleFollowUnfollow} isLoading={updating}>
+                    {following ? "Unfollow" : "Follow"}
+                </Button>
+            )}
+
             <Flex w={"full"} justifyContent={"space-between"}>
                 <Flex gap={2} alignItems={"center"}>
                     <Text color={"gray.light"}>{user?.followers.length} followers</Text>

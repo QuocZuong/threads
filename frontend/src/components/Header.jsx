361689c1c6424aeb387844e33c95fe9dbf0dbd60
Zuong <quocvuongle.ct@gmail.com>
2023-11-26 13:51:06 +0700

    update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,20 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Flex, Image, useColorMode, Link } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import userAtom from "../atoms/userAtom";
+import { Link as RouterLink } from "react-router-dom";
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode();
+    const user = useRecoilValue(userAtom);
+
     return (
-        <Flex justifyContent={"center"} mt={6} mb={12}>
+        <Flex justifyContent={"center"} gap={20} mt={6} mb={12}>
+            {user && (
+                <Link as={RouterLink} to="/">
+                    <AiFillHome size={24} />
+                </Link>
+            )}
             <Image
                 cursor={"pointer"}
                 alt="logo"
@@ -10,6 +22,12 @@ const Header = () => {
                 src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
                 onClick={toggleColorMode}
             ></Image>
+
+            {user && (
+                <Link as={RouterLink} to={`/${user.username}`}>
+                    <RxAvatar size={24} />
+                </Link>
+            )}
         </Flex>
     );
 };

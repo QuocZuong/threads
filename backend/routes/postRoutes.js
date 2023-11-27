f0e083201e59a1da799b174c02b0177d90cf1fc0
Zuong <quocvuongle.ct@gmail.com>
2023-11-27 11:54:10 +0700

    add new route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -6,13 +6,15 @@ import {
     likeUnlikePost,
     replyToPost,
     getFeedPost,
+    getUserPost,
 } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 router.get("/feed", protectRoute, getFeedPost);
 router.post("/", protectRoute, createPost);
-router.get("/:id", protectRoute, getPost);
+router.get("/:id", getPost); // get all posts
+router.get("/user/:username", protectRoute, getUserPost); // get user's posts
 router.delete("/:id", protectRoute, deletePost);
 router.put("/like/:id", protectRoute, likeUnlikePost);
 router.put("/reply/:id", protectRoute, replyToPost);

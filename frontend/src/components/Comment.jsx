d993ab07ee48192c8de84cc814e02a6196426a78
Zuong <quocvuongle.ct@gmail.com>
2023-11-27 14:02:31 +0700

    update comment

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
 import { Flex, Text, Divider } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/avatar";
-import { BsThreeDots } from "react-icons/bs";
-import Actions from "./Actions";
-const Comment = ({ comment, createdAt, likes, username, userAvatar }) => {
-    const [liked, setLiked] = useState(false);
-
+const Comment = ({ reply, lastReply }) => {
     return (
         <>
             <Flex gap={4} py={2} my={2} w={"full"}>
-                <Avatar src={userAvatar}></Avatar>
+                <Avatar src={reply?.userProfilePic}></Avatar>
                 <Flex gap={1} w={"full"} flexDirection={"column"}>
                     <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
                         <Text fontSize={"sm"} fontWeight={"bold"}>
-                            {username}
+                            {reply?.username}
                         </Text>
-                        <Flex gap={2} alignItems={"center"}>
-                            <Text fontSize={"sm"} color={"gray.light"}>
-                                {createdAt}
-                            </Text>
-                            <BsThreeDots />
-                        </Flex>
                     </Flex>
-                    <Text>{comment}</Text>
-                    <Actions liked={liked} setLiked={setLiked} />
-                    <Text fontSize={"sm"} color={"gray.light"}>
-                        {likes + (liked ? 1 : 0)} likes
-                    </Text>
+                    <Text>{reply?.text}</Text>
                 </Flex>
             </Flex>
-            <Divider my={4} />
+            {!lastReply ? <Divider my={4} /> : null}
         </>
     );
 };

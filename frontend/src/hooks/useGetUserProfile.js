0fd188d3798e2f28e83394cfa6ae920a3018a978
Zuong <quocvuongle.ct@gmail.com>
2023-11-27 14:02:59 +0700

    add hook useGetUserProfile

---
+++ frontend/src/hooks/useGetUserProfile.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useShowToast from "./useShowToast";
+
+const useGetUserProfile = () => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const { username } = useParams();
+    const showToast = useShowToast();
+
+    useEffect(() => {
+        const getUser = async () => {
+            setIsLoading(true);
+            try {
+                const res = await fetch(`/api/users/${username}`);
+                const data = await res.json();
+
+                if (data.error) {
+                    showToast("Error", data.error, "error");
+                    return;
+                }
+                setUser(data);
+            } catch (error) {
+                showToast("Error", error.message, "error");
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        getUser();
+    }, [username, showToast]);
+
+    return { isLoading, user };
+};
+
+export default useGetUserProfile;

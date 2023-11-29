0ef2aa5b57a2ac896cd4b4b6ef3f8678a6ce5d30
Zuong <quocvuongle.ct@gmail.com>
2023-11-29 11:26:57 +0700

    update title auth page

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -2,7 +2,11 @@ import { useRecoilValue } from "recoil";
 import authScreenAtom from "../atoms/authAtom";
 import Login from "../components/LoginCard";
 import SignupCard from "../components/SignupCard";
+import { useEffect } from "react";
 const AuthPage = () => {
+    useEffect(() => {
+        document.title = "Authentication";
+    }, []);
     const authScreenState = useRecoilValue(authScreenAtom);
     return <>{authScreenState === "login" ? <Login /> : <SignupCard />}</>;
 };

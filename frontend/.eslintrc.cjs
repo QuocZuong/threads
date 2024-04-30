1c99e0d7dba4a9e77f92988aa8313ff04c7603a3
Quoc Vuong <quocvuongle.ct@gmail.com>
2024-04-30 23:01:12 +0700

    Fix eslint warning for unused variables in .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -14,5 +14,6 @@ module.exports = {
     rules: {
         "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
         "react/prop-types": "off",
+        "no-unused-vars": "warn",
     },
 };

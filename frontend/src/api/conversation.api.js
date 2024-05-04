4e7333f2e7723515d2dd5c233e3737511c09a50d
Quoc Vuong <quocvuongle.ct@gmail.com>
2024-05-04 15:16:37 +0700

    add some hooks

---
+++ frontend/src/api/conversation.api.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_CONVERSATIONS_KEY } from "../constants/query.constants";
+
+export const getAllConversations = async () => {
+    const res = await fetch("/api/conversations");
+
+    if (!res.ok) {
+        throw new Error("Something went wrong");
+    }
+
+    const data = await res.json();
+    return data;
+};
+
+export const useGetAllConversations = () => {
+    return useQuery({
+        queryKey: QUERY_CONVERSATIONS_KEY,
+        queryFn: getAllConversations,
+    });
+};

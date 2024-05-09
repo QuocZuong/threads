89bb82fc3666728b5faf2b4e267fe6c944ca869f
Truc Vy <vymntce171243@fpt.edu.vn>
2024-05-09 21:30:20 +0700

    Modified - modified update profile UI

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -93,7 +93,7 @@ const UserHeader = ({ user }) => {
                     <Text color={"gray.light"}>{user?.followers.length} followers</Text>
                 </Flex>
                 <Flex gap={2} alignItems={"center"}>
-                    <Box className={isDarkMode ? "icon-container" : "icon-container_light"}>
+                    <Box className={isDarkMode ? "icon-container" : "icon-container_light"} padding={"8px 0px 8px 8px"}>
                         <BsInstagram size={24} cursor={"pointer"} />
                     </Box>
                 </Flex>

76fc8050313f268ca9c674ad6f50aed02e01f33e
BaoGfpt <hoduonggiabao1512@gmail.com>
2024-06-05 19:30:10 +0700

    fix issue 16 and issue 19

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
 import {
-    Flex,
-    Text,
-    Box,
-    Modal,
-    Button,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    FormControl,
-    Input,
+  Flex,
+  Text,
+  Box,
+  Modal,
+  Button,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  Input,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
@@ -21,227 +21,247 @@ import userAtom from "../atoms/userAtom";
 import postsAtom from "../atoms/postsAtom";
 import useShowToast from "../hooks/useShowToast";
 
-const isDarkMode = localStorage.getItem("chakra-ui-color-mode") === "dark";
-
 const Actions = ({ post }) => {
-    const user = useRecoilValue(userAtom);
-    const showToast = useShowToast();
-    const [liked, setLiked] = useState(post?.likes.includes(user?._id));
-    const [reply, setReply] = useState("");
-    const [posts, setPosts] = useRecoilState(postsAtom);
-    const [isLiking, setIsLiking] = useState(false);
-    const [isReplying, setIsReplying] = useState(false);
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const isDarkMode = localStorage.getItem("chakra-ui-color-mode") === "dark";
+  const user = useRecoilValue(userAtom);
+  const showToast = useShowToast();
+  const [liked, setLiked] = useState(post?.likes.includes(user?._id));
+  const [reply, setReply] = useState("");
+  const [posts, setPosts] = useRecoilState(postsAtom);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isReplying, setIsReplying] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const handleLikeAndUnlike = async () => {
-        if (!user) return showToast("Error", "You must be logged in to like a post", "error");
-        if (isLiking) return;
-        setIsLiking(true);
-        try {
-            const res = await fetch("/api/posts/like/" + post._id, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+  const handleLikeAndUnlike = async () => {
+    if (!user)
+      return showToast(
+        "Error",
+        "You must be logged in to like a post",
+        "error"
+      );
+    if (isLiking) return;
+    setIsLiking(true);
+    try {
+      const res = await fetch("/api/posts/like/" + post._id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-            const data = await res.json();
-            if (data.error) {
-                showToast("Error", data.error, "error");
-                return;
-            }
-            if (!liked) {
-                const updatedPosts = posts.map((p) => {
-                    if (p._id === post._id) {
-                        return { ...p, likes: [...p.likes, user._id] };
-                    }
-                    return p;
-                });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+      if (!liked) {
+        const updatedPosts = posts.map((p) => {
+          if (p._id === post._id) {
+            return { ...p, likes: [...p.likes, user._id] };
+          }
+          return p;
+        });
 
-                setPosts(updatedPosts);
-            } else {
-                const updatedPosts = posts.map((p) => {
-                    if (p._id === post._id) {
-                        return { ...p, likes: p?.likes?.filter((id) => id !== user._id) };
-                    }
-                    return p;
-                });
+        setPosts(updatedPosts);
+      } else {
+        const updatedPosts = posts.map((p) => {
+          if (p._id === post._id) {
+            return { ...p, likes: p?.likes?.filter((id) => id !== user._id) };
+          }
+          return p;
+        });
 
-                setPosts(updatedPosts);
-            }
+        setPosts(updatedPosts);
+      }
 
-            setLiked(!liked);
-        } catch (error) {
-            showToast("Error", error, "error");
-        } finally {
-            setIsLiking(false);
+      setLiked(!liked);
+    } catch (error) {
+      showToast("Error", error, "error");
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
+  const handleReply = async () => {
+    if (!user)
+      return showToast(
+        "Error",
+        "You must be logged in to reply a post",
+        "error"
+      );
+    if (isReplying) return;
+    setIsReplying(true);
+    try {
+      const res = await fetch("/api/posts/reply/" + post._id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: reply }),
+      });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+
+      const updatePosts = posts.map((p) => {
+        if (p._id === post._id) {
+          return { ...p, replies: [...p.replies, data] };
         }
-    };
+        return p;
+      });
 
-    const handleReply = async () => {
-        if (!user) return showToast("Error", "You must be logged in to reply a post", "error");
-        if (isReplying) return;
-        setIsReplying(true);
-        try {
-            const res = await fetch("/api/posts/reply/" + post._id, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ text: reply }),
-            });
-            const data = await res.json();
-            if (data.error) {
-                showToast("Error", data.error, "error");
-                return;
-            }
+      setPosts(updatePosts);
 
-            const updatePosts = posts.map((p) => {
-                if (p._id === post._id) {
-                    return { ...p, replies: [...p.replies, data] };
-                }
-                return p;
-            });
+      showToast("Success", "Reply successfully", "success");
+      onClose();
+      setReply("");
+    } catch (error) {
+      showToast("Error", error, "error");
+    } finally {
+      setIsReplying(false);
+    }
+  };
 
-            setPosts(updatePosts);
+  return (
+    <Flex flexDirection="column">
+      <Flex gap={3} my={1} onClick={(e) => e.preventDefault()} className="">
+        <svg
+          aria-label="Like"
+          color={liked ? "rgb(237, 73, 86)" : ""}
+          fill={liked ? "rgb(237, 73, 86)" : "transparent"}
+          role="img"
+          viewBox="0 0 24 22"
+          onClick={handleLikeAndUnlike}
+          className={isDarkMode ? "icon-container" : "icon-container_light"}
+        >
+          <path
+            d="M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z"
+            stroke="currentColor"
+            strokeWidth="2"
+          ></path>
+        </svg>
 
-            showToast("Success", "Reply successfully", "success");
-            onClose();
-            setReply("");
-        } catch (error) {
-            showToast("Error", error, "error");
-        } finally {
-            setIsReplying(false);
-        }
-    };
+        <svg
+          aria-label="Comment"
+          color=""
+          fill=""
+          role="img"
+          viewBox="0 0 24 24"
+          className={isDarkMode ? "icon-container" : "icon-container_light"}
+          onClick={onOpen}
+        >
+          <title>Comment</title>
+          <path
+            d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
+            fill="none"
+            stroke="currentColor"
+            strokeLinejoin="round"
+            strokeWidth="2"
+          ></path>
+        </svg>
 
-    return (
-        <Flex flexDirection="column">
-            <Flex gap={3} my={1} onClick={(e) => e.preventDefault()} className="">
-                <svg
-                    aria-label="Like"
-                    color={liked ? "rgb(237, 73, 86)" : ""}
-                    fill={liked ? "rgb(237, 73, 86)" : "transparent"}
-                    role="img"
-                    viewBox="0 0 24 22"
-                    onClick={handleLikeAndUnlike}
-                    className={isDarkMode ? "icon-container" : "icon-container_light"}
-                >
-                    <path
-                        d="M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                    ></path>
-                </svg>
+        <RepostSVG />
+        <ShareSVG />
+      </Flex>
 
-                <svg
-                    aria-label="Comment"
-                    color=""
-                    fill=""
-                    role="img"
-                    viewBox="0 0 24 24"
-                    className={isDarkMode ? "icon-container" : "icon-container_light"}
-                    onClick={onOpen}
-                >
-                    <title>Comment</title>
-                    <path
-                        d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                    ></path>
-                </svg>
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize="sm">
+          {post?.replies?.length}{" "}
+          {post?.replies?.length > 1 ? "replies" : "reply"}
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize="sm">
+          {post?.likes?.length} {post?.likes?.length > 1 ? "likes" : "like"}
+        </Text>
+      </Flex>
 
-                <RepostSVG />
-                <ShareSVG />
-            </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Reply</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <Input
+                placeholder="Reply goes here..."
+                value={reply}
+                onChange={(e) => setReply(e.target.value)}
+              ></Input>
+            </FormControl>
+          </ModalBody>
 
-            <Flex gap={2} alignItems={"center"}>
-                <Text color={"gray.light"} fontSize="sm">
-                    {post?.replies?.length} {post?.replies?.length > 1 ? "replies" : "reply"}
-                </Text>
-                <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-                <Text color={"gray.light"} fontSize="sm">
-                    {post?.likes?.length} {post?.likes?.length > 1 ? "likes" : "like"}
-                </Text>
-            </Flex>
-
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Reply</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <Input
-                                placeholder="Reply goes here..."
-                                value={reply}
-                                onChange={(e) => setReply(e.target.value)}
-                            ></Input>
-                        </FormControl>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme="blue" size={"sm"} mr={3} onClick={handleReply} isLoading={isReplying}>
-                            Reply
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Flex>
-    );
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              size={"sm"}
+              mr={3}
+              onClick={handleReply}
+              isLoading={isReplying}
+            >
+              Reply
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Flex>
+  );
 };
 
 export default Actions;
 
 const RepostSVG = () => {
-    return (
-        <svg
-            aria-label="Repost"
-            color="currentColor"
-            fill="currentColor"
-            role="img"
-            viewBox="0 0 24 24"
-            className={isDarkMode ? "icon-container" : "icon-container_light"}
-        >
-            <title>Repost</title>
-            <path
-                fill=""
-                d="M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z"
-            ></path>
-        </svg>
-    );
+  const isDarkMode = localStorage.getItem("chakra-ui-color-mode") === "dark";
+
+  return (
+    <svg
+      aria-label="Repost"
+      color="currentColor"
+      fill="currentColor"
+      role="img"
+      viewBox="0 0 24 24"
+      className={isDarkMode ? "icon-container" : "icon-container_light"}
+    >
+      <title>Repost</title>
+      <path
+        fill=""
+        d="M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1.004 1.004 0 0 0-.294.707v.001c0 .023.012.042.013.065a.923.923 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z"
+      ></path>
+    </svg>
+  );
 };
 
 const ShareSVG = () => {
-    return (
-        <svg
-            aria-label="Share"
-            color=""
-            fill="rgb(243, 245, 247)"
-            role="img"
-            viewBox="0 0 24 24"
-            className={isDarkMode ? "icon-container" : "icon-container_light"}
-        >
-            <title>Share</title>
-            <line
-                fill="none"
-                stroke="currentColor"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                x1="22"
-                x2="9.218"
-                y1="3"
-                y2="10.083"
-            ></line>
-            <polygon
-                fill="none"
-                points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
-                stroke="currentColor"
-                strokeLinejoin="round"
-                strokeWidth="2"
-            ></polygon>
-        </svg>
-    );
+  const isDarkMode = localStorage.getItem("chakra-ui-color-mode") === "dark";
+
+  return (
+    <svg
+      aria-label="Share"
+      color=""
+      fill="rgb(243, 245, 247)"
+      role="img"
+      viewBox="0 0 24 24"
+      className={isDarkMode ? "icon-container" : "icon-container_light"}
+    >
+      <title>Share</title>
+      <line
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        x1="22"
+        x2="9.218"
+        y1="3"
+        y2="10.083"
+      ></line>
+      <polygon
+        fill="none"
+        points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      ></polygon>
+    </svg>
+  );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,82 +14,96 @@ import userAtom from "../atoms/userAtom";
 import { Link as RouterLink } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
 import { SearchIcon } from "@chakra-ui/icons";
+import "../components/Header.css";
 
 import authScreenAtom from "../atoms/authAtom";
 const Header = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const user = useRecoilValue(userAtom);
-    const logout = useLogout();
-    const setAuthScreen = useSetRecoilState(authScreenAtom);
-    const isDarkMode = localStorage.getItem("chakra-ui-color-mode") === "dark";
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+  const logout = useLogout();
+  const setAuthScreen = useSetRecoilState(authScreenAtom);
+  const isDarkMode = localStorage.getItem("chakra-ui-color-mode") === "dark";
 
-    return (
-        <Flex justifyContent={"center"} gap={10} mt={4} mb={8}>
-            {user && (
-                <Link as={RouterLink} to="/">
-                    <GrHomeRounded size={50} className={isDarkMode ? "icon-container" : "icon-container_light"} />
-                </Link>
-            )}
+  return (
+    <Flex justifyContent={"space-between"} mt={4} mb={8}>
+      {user && (
+        <Link as={RouterLink} to="/">
+          <GrHomeRounded
+            size={50}
+            className={isDarkMode ? "icon-container" : "icon-container_light"}
+          />
+        </Link>
+      )}
 
-            {!user && (
-                <Link as={RouterLink} to="/" onClick={() => setAuthScreen("login")}>
-                    Login
-                </Link>
-            )}
+      {!user && (
+        <Link as={RouterLink} to="/" onClick={() => setAuthScreen("login")}>
+          Login
+        </Link>
+      )}
 
-            {user && (
-                <Link as={RouterLink} to="/chat">
-                    <BsFillChatSquareQuoteFill
-                        size={35}
-                        className={isDarkMode ? "icon-container" : "icon-container_light"}
-                    />
-                </Link>
-            )}
+      {user && (
+        <Link as={RouterLink} to="/chat">
+          <BsFillChatSquareQuoteFill
+            size={35}
+            className={isDarkMode ? "icon-container" : "icon-container_light"}
+          />
+        </Link>
+      )}
 
-            {user && (
-                <Link as={RouterLink} to="/chat">
-                    <BsFillChatSquareQuoteFill
-                        size={35}
-                        className={isDarkMode ? "icon-container" : "icon-container_light"}
-                    />
-                </Link>
-            )}
-            <Image
-                cursor={"pointer"}
-                alt="logo"
-                w={50}
-                src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-                className={isDarkMode ? "icon-container" : "icon-container_light"}
-                onClick={toggleColorMode}
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={50}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+        className={isDarkMode ? "icon-container" : "icon-container_light"}
+        onClick={toggleColorMode}
+      />
+      <RouterLink to="/search">
+        <SearchIcon
+          boxSize={35}
+          className={isDarkMode ? "icon-container" : "icon-container_light"}
+        ></SearchIcon>
+      </RouterLink>
+      <LuPenSquare
+        size={50}
+        className={isDarkMode ? "icon-container" : "icon-container_light"}
+      />
+      <BsHeart
+        size={50}
+        strokeWidth={0.3}
+        className={isDarkMode ? "icon-container" : "icon-container_light"}
+      />
+      {user && (
+        <Flex alignItems={"center"} gap={4}>
+          <Link as={RouterLink} to={`/${user.username}`}>
+            <FaRegUser
+              size={24}
+              className={isDarkMode ? "icon-container" : "icon-container_light"}
+              cursor={"pointer"}
             />
-            <RouterLink to="/search">
-                <SearchIcon boxSize={35} className={isDarkMode ? "icon-container" : "icon-container_light"}></SearchIcon>
-            </RouterLink>
-            <LuPenSquare size={50} className={isDarkMode ? "icon-container" : "icon-container_light"} />
-            <BsHeart size={50} strokeWidth={0.3} className={isDarkMode ? "icon-container" : "icon-container_light"} />
-            {user && (
-                <Flex alignItems={"center"} gap={4}>
-                    <Link as={RouterLink} to={`/${user.username}`}>
-                        <FaRegUser size={24} className={isDarkMode ? "icon-container" : "icon-container_light"} cursor={"pointer"}/>
-                    </Link>
-                </Flex>
-            )}
-
-            {!user && (
-                <Link as={RouterLink} to="/auth" onClick={() => setAuthScreen("signup")}>
-                    Sign up
-                </Link>
-            )}
-            {user && (
-                <FiLogOut
-                    size={35}
-                    className={isDarkMode ? "icon-container" : "icon-container_light"}
-                    onClick={logout}
-                    cursor={"pointer"}
-                />
-            )}
+          </Link>
         </Flex>
-    );
+      )}
+
+      {!user && (
+        <Link
+          as={RouterLink}
+          to="/auth"
+          onClick={() => setAuthScreen("signup")}
+        >
+          Sign up
+        </Link>
+      )}
+      {user && (
+        <FiLogOut
+          size={35}
+          className={isDarkMode ? "icon-container" : "icon-container_light"}
+          onClick={logout}
+          cursor={"pointer"}
+        />
+      )}
+    </Flex>
+  );
 };
 
 export default Header;
